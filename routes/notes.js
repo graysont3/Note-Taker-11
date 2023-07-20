22c356ec34b7a77725a130c37f56a44fa8b1b06a
Star Child <tajagrayson@Tajas-MacBook-Air.local>
2023-07-19 22:00:48 -0400

    completed notes route

--- routes/notes.js
+++ routes/notes.js
@@ -1,1 +1,33 @@
- 
+ const notes = require('express').Router();
+ const uuid = require('uuid');
+ const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
+
+
+ notes.get('/', (req, res) =>
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+ );
+
+ notes.post('/', (req, res) => {
+    const { noteTitle, noteText } = req.body;
+
+    if (noteTitle && noteText ) {
+        const newNote = {
+            newTitle,
+            newText,
+            id: uuid(),
+        };
+
+        readAndAppend(newNotes, './db/db.json');
+
+        const data = {
+            status: 'success',
+            body: newNote,
+        };
+
+        res.json(data);
+    } else {
+        res.json('Error! Could not post Note!')
+    }
+ });
+
+ module.exports = notes; 
